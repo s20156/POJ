a78eb655bc693cc12a17f51ef310213728239ac1
Mateusz Grube <s20156@pjwstk.edu.pl>
2020-02-22 12:59:57 +0100

    Clean package system

--- src/pl/pjatk/Zjazd1/Zad1/Zad1.java
+++ src/pl/pjatk/Zjazd1/Zad1/Zad1.java
@@ -1,11 +1,14 @@
 package pl.pjatk.Zjazd1.Zad1;
 
-public class Zad1 {
-    byte a = 68;
-    short b = 267;
-    int c = -637;
+public class Main {
 
-    long i = (long) (50000 + 10 * (a + b + c));
+    public static void main(String[] args) {
+        byte a = 68;
+        short b = 267;
+        int c = -637;
+
+        long i = (long) (50000 + 10 * (a + b + c));
 
         System.out.println(i);
+    }
 }
--- src/pl/pjatk/Zjazd1/Zad2/Zad2.java
+++ src/pl/pjatk/Zjazd1/Zad2/Zad2.java
@@ -1,11 +1,14 @@
 package pl.pjatk.Zjazd1.Zad2;
 
-public class Zad2 {
-    double a = 20;
-    double b = 40;
-    double res = (a+b)*100;
-    boolean wynik = (res % 40.0d) == 0;
+public class Main {
+
+    public static void main(String[] args) {
+        double a = 20;
+        double b = 40;
+        double res = (a+b)*100;
+        boolean wynik = (res % 40.0d) == 0;
         if (!wynik) {
-        System.out.println("Modulo higher than 0");
+            System.out.println("Modulo higher than 0");
+        }
     }
 }
